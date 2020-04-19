4d3ba3b49bd12adf7d08aebb9daa4691652257b1
Kelly Bazella <kelly.bazella@gmail.com>
2020-04-18 22:23:52 -0500

    added readme

--- Develop/README.md
+++ Develop/README.md
@@ -1,1 +1,19 @@
 # Workout-Tracker
+
+## User Story
+As a user, I want to be able to create workouts as well as view stats from workouts. I want to be able to add exercises by including the name, duration, type, weight, sets, and reps so I can keep track of my process. 
+
+## Description
+
+This app allows its users the ability to track their workouts. They can add exercises they complete and keep an accurate record. 
+
+## Stack
+This application was created using:
+- Node.js
+- Mongoose
+- MongoDb
+
+## Links
+
+[Heroku Deployment](https://bazella-fitness-tracker.herokuapp.com/)
+[GitHub Repository](https://github.com/kelly-bazella/Workout-Tracker)
--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const mongojs = require("mongojs");
 const Workout = require("../models/workout.js");
 module.exports = function (app) {
   app.get("/api/workouts", function (req, res) {
@@ -12,15 +13,44 @@ module.exports = function (app) {
   });
 
   app.post("/api/workouts", function (req, res) {
-    Workout.create({})
-      .then(function (data) {
-        res.json(data);
-      })
-      .catch(function (err) {
-        res.json(err);
+    Workout.insert(req.body, function(error,data){
+      if(error){
+        res.send(error)
+      }else{
+        res.send(data)
+      }
+    })
+  });
+
+  app.put("/api/workouts/:id", function(req, res){
+      Workout.update(
+        {
+          _id: mongojs.ObjectId(req.params.id)
+      }, 
+      {
+        $set: {
+            exercise: req.body.exercise,
+            name: req.body.name,
+            distance: req.body.distance,
+            duration: req.body.distance,
+            weight: req.body.weight,
+            sets: req.body.sets,
+            reps: req.body.reps
+        }
+      }, function(err, data){
+          if (err){
+              res.send(err)
+          }else{
+              res.send(data)
+          }
       });
   });
 
-  
-
+app.get("/api/workouts/range", function(req, res){
+    Workout.find({}).limit(7).then(function(data){
+        res.json(data);
+    }).catch(function(err){
+        res.json(err)
+    });
+});
 };
